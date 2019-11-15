d5862632ff60f3e0e50c1101287a3b80f2627714
Julián Unrrein <junrrein@gmail.com>
2019-11-15 17:54:13 -0300

    Fem2d: poinload: Renombrar variables

--- Fem2d/fem2d_pstr_pointload.m
+++ Fem2d/fem2d_pstr_pointload.m
@@ -1,23 +1,30 @@
 function [F] = fem2d_pstr_pointload(F,pointload,xnode,icone)
+    
+    esTriangular = @(elemento) icone(elemento,4) == -1;
+    
     for n = 1 : size(pointload,1)
-        e = pointload(n,1);
+        elemento = pointload(n,1);
         Fx = pointload(n,2);
         Fy = pointload(n,3);
         xp = pointload(n,4);
         yp = pointload(n,5);
-        if icone(e,4) == -1 % triangular element
-            ele = icone(e,1:3);
+        
+        if esTriangular(elemento)
+            indicesNodos = icone(elemento,1:3);
         else
-            ele = icone(e,:);
+            indicesNodos = icone(elemento,:);
         end
-        nodes = xnode(ele,:);
-        N = fem2d_pstr_blerp(nodes,xp,yp);
+        
+        nodos = xnode(indicesNodos,:);
+        N = fem2d_pstr_blerp(nodos,xp,yp);
         indx = [];
         f = [];
-        for i=1:length(ele)
-            indx = [indx ele(i)*2-1 ele(i)*2];
+        
+        for i = 1 : length(indicesNodos)
+            indx = [indx indicesNodos(i)*2-1 indicesNodos(i)*2];
             f = [f N(i)*Fx N(i)*Fy];
         end
-        F(indx) = F(indx) + f';
+        
+        F(indx) += f';
     end
 end
