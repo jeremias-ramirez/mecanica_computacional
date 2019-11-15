80f6784ccb148f390c89e444c4dc69673a02c5b5
Julián Unrrein <junrrein@gmail.com>
2019-11-15 19:38:17 -0300

    Fem2d: genF: Resolver

--- Fem2d/fem2d_pstr_genF.m
+++ Fem2d/fem2d_pstr_genF.m
@@ -13,9 +13,35 @@ function [localF] = fem2d_pstr_genF(nodes,Fg,th)
 % * localF: vector local de fuerzas.
 % ----------------------------------------------------------------------
 
-% Para elementos triangulares, localF tiene que ser de 3x1
-% Para cuadrángulos, tiene que ser 4x1
+% Para elementos triangulares, localF tiene que ser de 6x1
+% Para cuadrángulos, tiene que ser 8x1
 
-    localF = [];
+    formanTriangulo = @(nodos) size(nodos,1) == 3;
+
+    if formanTriangulo(nodes)
+        nodo1 = nodes(1, :);
+        nodo2 = nodes(2, :);
+        nodo3 = nodes(3, :);
+        
+        vector1 = [nodo2-nodo1 0];
+        vector2 = [nodo3-nodo2 0];
+        A = norm(cross(vector1, vector2)) / 2;
+        
+        localF = -Fg * A * th / 3 * [0 1 0 1 0 1]';
+    else
+        nodo1 = nodes(1, :);
+        nodo2 = nodes(2, :);
+        nodo3 = nodes(3, :);
+        nodo4 = nodes(4, :);
+        
+        triang1vector1 = [nodo2-nodo1 0];
+        triang1vector2 = [nodo3-nodo2 0];
+        A1 = norm(cross(triang1vector1, triang1vector2)) / 2;
+        triang2vector1 = [nodo3-nodo1 0];
+        triang2vector2 = [nodo4-nodo3 0];
+        A2 = norm(cross(triang2vector1, triang2vector2)) / 2;
+        A = A1 + A2;
     
+        localF = -Fg * A * th / 4 * [0 1 0 1 0 1 0 1]';
+    end
 end
