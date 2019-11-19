89f8c8f54752151f6b3d67f9bace72124b625876
Jeremias <jeremiaslagorio@gmail.com>
2019-11-19 16:56:17 -0300

    Fem2d: Resolver const_mat

--- Fem2d/fem2d_pstr_const_mat.m
+++ Fem2d/fem2d_pstr_const_mat.m
@@ -11,6 +11,15 @@ function [D] = fem2d_pstr_const_mat(model)
 % * D: matriz constitutiva del sistema.
 % ----------------------------------------------------------------------
 
-    D = [];
-    
+    E = model.young;
+    v = model.poiss;
+
+    if (model.pstrs == 1) %considero que si es uno es Tension Plana 
+        D = E / (1- v^2) * [1 v 0 ; v 1 0 ; 0 0 (1-v)/2];
+    else
+        aux1 = E*(1-v) / ((1 +v ) * (1 - 2 * v));
+        aux2 = v / ( 1- v);
+        aux3 = (1 - 2 * v)/ (2 * (1-v));
+        D = aux1 * [1 aux2 0 ; aux2 1 0 ; 0 0 aux3];
+    end
 end
